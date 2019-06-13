a425c37e7200ef02a07b1084a9320b7aa397c4e8
zhoust <zhoust@guahao.com>
2019-06-13 10:46:20 +0800

    feat: 1.通过node去查询表是否存在

--- todolist/server/index.js
+++ todolist/server/index.js
@@ -11,17 +11,56 @@ app.use(bodyParser.json());
 //         message: 'success'
 //     })
 // })
-const connection = mysql.createConnection({
-    host: 'db',
-    user: 'docker',
-    port: '3306',
-    password: '123456',
-    database: 'todolist'
-});
+const opt = {
+    host: 'localhost',
+    user: 'root',
+    port: '3308',
+    password: '123456'
+};
+let connection = mysql.createConnection(opt);
 
-connection.connect(err => {
+const sqlFn = (sql) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, (err, results, filelds) => {
+            if (err) throw err;
+            resolve(results);
+        });
+    })
+}
+
+connection.connect(async (err) => {
     if (err) throw err;
     console.log('mysql connncted success!');
+    // console.log(connection)
+    // const findDatabase = `select t.table_name from information_schema.TABLES t where t.TABLE_SCHEMA ="database_name" and t.TABLE_NAME ='table_name'`
+    const findDatabase = `select count(*) from information_schema.TABLES t where t.TABLE_SCHEMA ="todolist"`
+    const data = await sqlFn(findDatabase);
+    console.log(data[0], data[0]['count(*)'], !Boolean(data[0]['count(*)']));
+    if (!data[0]['count(*)']) {
+        const result = await sqlFn('create database todolist');
+        connection = mysql.createConnection({...opt, ...{database: 'todolist'}});
+
+        // const sql = `CREATE TABLE list (
+        //     id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        //     text VARCHAR(255),
+        //     check INT(11) DEFAULT 0
+        //     )`;
+        // const result2 = await sqlFn(sql);
+        // console.log(result2, 'reslult2');
+    }
+})
+app.get('/sql', async (req, res) => {
+    const sql = req.query.sql;
+
+    const data = await sqlFn(sql);
+    const a = data.find(o => o.Tables_in_mysql === 'user');
+    console.log(data);
+    console.log(a)
+    res.json({
+        code: 0,
+        data,
+        message: 'success'
+    })
 })
 
 app.get('/getList', async (req, res) => {
